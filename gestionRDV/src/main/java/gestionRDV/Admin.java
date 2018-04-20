eab9fce354b1b106302f7cf3964ca83bd89926a0
bazicode <dr.mustaphaelbazi@gmail.com>
2018-04-20 13:05:06 +0100

    add hibernate to maven deps

---
+++ gestionRDV/src/main/java/gestionRDV/Admin.java
@@ -0,0 +1,43 @@
+package gestionRDV;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "admin")
+public class Admin {
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "username")
+	private String username;
+
+	@Column(name = "password")
+	private String password;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
